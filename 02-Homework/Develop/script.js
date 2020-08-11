ae35d6bd671baab110065589ebec63659a8b75dc
Scott Buonya <scottbuonya@gmail.com>
2020-08-10 21:36:15 -0400

    added arrays for time

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,3 +1,73 @@
-var m = moment();
-var currentDay = m.format("MMMM Do YYYY, h:mm:ss a");
-$("#currentDay").text(currentDay);
+var myDay = [
+    {
+        id: "0",
+        hour: "09",
+        time: "09",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: "1",
+        hour: "10",
+        time: "10",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: "2",
+        hour: "11",
+        time: "11",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: "3",
+        hour: "12",
+        time: "12",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "4",
+        hour: "01",
+        time: "13",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "5",
+        hour: "02",
+        time: "14",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "6",
+        hour: "03",
+        time: "15",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "7",
+        hour: "04",
+        time: "16",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "8",
+        hour: "05",
+        time:"17",
+        meridiem: "pm",
+        reminder: ""
+    },
+]
+console.log(myDay)
+
+
+function getHeaderDate() {
+var currentHeaderDate = moment().format("dddd, MMMM Do");
+$("#currentDay").text(currentHeaderDate);
+}
+getHeaderDate();
