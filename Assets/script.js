6edce8a29ffc14a9351b5732b2fdf0f0864a0b52
Scott Buonya <scottbuonya@gmail.com>
2020-08-30 19:53:12 -0400

    made my variables for moment and functions to save to local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,19 @@
-var currentDate = moment().format("dddd, MMMM Do");
+// using moment js 
+var currentDay = moment.format("dddd, MMMM Do");
+$("#currentDay").text(currentDay);
+
+// more variables 
+var hour = moment().format("H");
+var button = $(".saveBtn")
+var moment = moment();
+
+
+// click functions to save to local storage
+button.on("click", function () {
+    var dataInput = $(this).siblings(".description").val();
+    var rowEl = $(this).parent().attr("id");
+    localStorage.setItem(rowEl, userInput)
+});
 
 
 
