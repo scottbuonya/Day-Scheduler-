41b9788327d3de61f3d8ecf9d549256fbf9aa575
Scott Buonya <scottbuonya@gmail.com>
2020-08-30 20:07:47 -0400

    added the bring back value for each descritpion

--- Assets/script.js
+++ Assets/script.js
@@ -12,8 +12,18 @@ var moment = moment();
 button.on("click", function () {
     var dataInput = $(this).siblings(".description").val();
     var rowEl = $(this).parent().attr("id");
-    localStorage.setItem(rowEl, userInput)
+    localStorage.setItem(rowEl, dataInput)
 });
+
+$("#9 .description").val(localStorage.getItem("9"))
+$("#10 .description").val(localStorage.getItem("10"))
+$("#11 .description").val(localStorage.getItem("11"))
+$("#12 .description").val(localStorage.getItem("12"))
+$("#1 .description").val(localStorage.getItem("1"))
+$("#2 .description").val(localStorage.getItem("2"))
+$("#3 .description").val(localStorage.getItem("3"))
+$("#4 .description").val(localStorage.getItem("4"))
+$("#5 .description").val(localStorage.getItem("5"))
 
 
 
